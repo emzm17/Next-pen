9f27c8aa7699cdef896276c8177ccb433b20c277
emzm17 <dibya172000@yahoo.com>
2025-02-15 16:16:41 +0530

    add cors

---
+++ src/main/java/com/example/codeService/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.example.codeService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("https://next-pen-ui.vercel.app")
+                        .allowedMethods("GET", "POST", "OPTIONS", "DELETE", "PUT")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- src/main/java/com/example/codeService/controller/CodeController.java
+++ src/main/java/com/example/codeService/controller/CodeController.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000") // Frontend origin
 public class CodeController {
 
     private final CodeServiceImpl codeService;
